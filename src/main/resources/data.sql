c5b657878776843663f5315a92478a268e9a8b7b
Halil Demir <halil.demir@rewe-digital.com>
2022-01-30 23:29:19 +0100

    application.yml file and sql scripts are added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,25 @@
+TRUNCATE TABLE deliveries;
+
+INSERT INTO deliveries (delivery_id, product, supplier, quantity, expected_date, expected_warehouse,received)
+VALUES (101, 'Bananas','JungleInc',1000000,'2027-01-08T07:17:48.237Z','TheMoon',0);
+
+INSERT INTO deliveries (delivery_id, product, supplier, quantity, expected_date, expected_warehouse,received)
+VALUES (102, 'Saiyans','Bardock',9001,'2019-10-10T09:08:11.098Z','Namek',0);
+
+INSERT INTO deliveries (delivery_id, product, supplier, quantity, expected_date, expected_warehouse,received)
+VALUES (103, 'Skull, Crystal','Akator',1,'2008-05-22T00:00:00.001Z','Headquarters',0);
+
+INSERT INTO deliveries (delivery_id, product, supplier, quantity, expected_date, expected_warehouse,received)
+VALUES (104, 'Bananas','JungleInc',1,'2020-10-08T07:18:42.937Z','BerlinZoo',0);
+
+INSERT INTO deliveries (delivery_id, product, supplier, quantity, expected_date, expected_warehouse,received)
+VALUES (105, 'Apples','ApplesToOrangesInc',50,'2020-05-01T09:00:42.000Z','Oranges',0);
+
+INSERT INTO deliveries (delivery_id, product, supplier, quantity, expected_date, expected_warehouse,received)
+VALUES (106, 'Salad','HealthyFoodInc',600,'2021-11-01T11:20:42.000Z','Headquarters',0);
+
+INSERT INTO deliveries (delivery_id, product, supplier, quantity, expected_date, expected_warehouse,received)
+VALUES (107, 'Salad','JungleInc',400,'2021-11-02T11:20:42.000Z','Headquarters',0);
+
+INSERT INTO deliveries (delivery_id, product, supplier, quantity, expected_date, expected_warehouse,received)
+VALUES (108, 'Salad','JungleInc',800,'2021-11-03T11:20:42.000Z','TheMoon',0);
